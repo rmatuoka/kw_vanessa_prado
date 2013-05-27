cfe9ad8321895bbc8fa3e24bab6e4ba6529fe545
-== Zaba ==- <bsilva@korewa.com.br>
2013-05-27 20:23:32 -0300

    Ajustes Finais Vanessa Prado

--- app/controllers/resellers_controller.rb
+++ app/controllers/resellers_controller.rb
@@ -7,7 +7,7 @@ class ResellersController < ApplicationController
     @reseller = Resale.new
   end
   def create
-    @reseller = Resale.new(params[:reseller])
+    @reseller = Resale.new(params[:resale])
     if @reseller.save
       ResellerMailer.send_reseller_to_adm(@reseller).deliver
       redirect_to reseller_path(@reseller), :notice => "Mensagem enviada com sucesso."
@@ -17,5 +17,6 @@ class ResellersController < ApplicationController
     end
   end
   def show
+   # @reseller = Resale.find_by_id(params[:id])
   end
 end
