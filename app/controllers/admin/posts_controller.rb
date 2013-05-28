3316689a045b806d251ed5d5618a0eb6d66bbb74
-== Zaba ==- <bsilva@korewa.com.br>
2013-05-27 22:04:31 -0300

    Tirando o Tiny

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,20 +3,6 @@ access_control do
       allow :admin, :all
 end
   layout "inadmin"
-  uses_tiny_mce :options => {
-                              :theme => 'advanced',
-                              :theme_advanced_resizing => true,
-                              :theme_advanced_resize_horizontal => false,
-                              :theme_advanced_buttons1 => %w{ newdocument | bold italic underline strikethrough | justifyleft justifycenter justifyright justifyfull styleselect formatselect fontselect fontsizeselect },
-                              :theme_advanced_buttons2 => %w{ cut copy paste pastetext pasteword | search replace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image cleanup help code | insertdate inserttime preview | forecolor backcolor},
-                              :theme_advanced_buttons3 => %w{ tablecontrols | hr removeformat visualaid | sub sup | charmap emotions iespell media advhr | print | ltr rtl | fullscreen},
-                              :theme_advanced_buttons4 => %w{ insertlayer moveforward movebackward absolute | styleprops | cite abbr acronym del ins attribs | visualchars nonbreaking pagebreak},
-                              #:plugins => %w{ table fullscreen }
-                              :extended_valid_elements => "iframe[src|width|height|name|align]",
-                              :plugins => %w{ safari pagebreak style layer table save advhr advimage advlink emotions iespell inlinepopups insertdatetime preview media searchreplace print contextmenu paste directionality fullscreen noneditable visualchars nonbreaking xhtmlxtras template},
-                              :width =>"100%",
-                            	:height =>"400"
-                            }  
   def index
     @posts = Post.all
   end
